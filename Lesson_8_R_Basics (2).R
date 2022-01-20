86b62b89aa273c2c8c411a8addd34707ff7fa6bb
JEMacGibbon <95629823+JEMacGibbon@users.noreply.github.com>
2022-01-20 16:07:14 -0500

    Add files via upload

---
+++ Lesson_8_R_Basics (2).R
@@ -0,0 +1,55 @@
+## SE370 AY 22-2
+## Lesson 8: R and Rstudio Basics
+
+# Make sure you have cloned this project in R Studio Cloud
+
+# Follow the prompts on this script. Once complete, export this script 
+# and upload it to your Teams assignment. This will count as your 
+# submission for Lesson 9
+
+## Basic Math
+
+# Add 4 and 8
+
+
+# Add 6 and 12, and assign the result of this addition to the variable x
+
+
+# Subtract 3 from 5
+
+
+# Multiply 3 by 5
+
+
+# Divide 12 by 4
+
+
+# Raise 5 to the 3rd power
+
+
+# Find the modulo (remainder) of twenty-eight and six
+
+
+# Assign the value 7 to the variable y
+
+
+# Subtract y from x
+
+
+# Multiply x by y, and assign the result to the variable z
+
+
+
+
+
+
+
+## Cloud Computing
+
+# In your own words, describe what cloud computing is.
+
+
+# What are the benefits of cloud computing? What are the drawbacks?
+
+
+
